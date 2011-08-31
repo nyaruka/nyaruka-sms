80d92a6fde59859a1702cf264f3c4b6424bec6d3
Nic Pottier <nicpottier@gmail.com>
2011-08-31 20:31:38 +0200

    beautify db list page

--- assets/apps/echo/main.js
+++ assets/apps/echo/main.js
@@ -5,12 +5,12 @@ function echo(req, resp){
     if (req.method == 'POST'){
         console.log("POST: " + JSON.stringify(req.params));
         if (req.params.first && req.params.last){
-            console.log("SAVING");
             db.contacts.save(req.params);
         }
     }
 
     resp.set('url', resp.url);
+    resp.set('method', resp.method);
     resp.set('contacts', db.contacts.find({}).all());
 }
 
